539de025fbdec7306c57e0c2eda5d22e39a38d92
Claire Chang <claires.chang@gmail.com>
2021-05-11 19:38:15 -0700

    flowchart

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ### Some code drawn from...
 *   Timeline Scribble for the timeline format https://codepen.io/Fischaela/pen/sJdqg/
+*   w3schools for the scroll to top button https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
 
 ### Design inspiration from...
 *   http://www.garysheng.com/
--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //Get the button:
-mybutton = document.getElementById("topBtn");
+topbutton = document.getElementById("topBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    topbutton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    topbutton.style.display = "none";
   }
 }
 
